bb6651436dc00663bd0072679641756ed53bb386
Ben Chen <cfangzheng@gmail.com>
2021-10-11 18:14:35 +0800

    feat: add costom select component of id-select.tsx

--- src/components/id-select.tsx
+++ src/components/id-select.tsx
@@ -1,8 +1,37 @@
+import { Select } from "antd";
 
-export const IdSelect = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+type SelectProps = React.ComponentProps<typeof Select>
+interface IdSelectProps extends Omit<SelectProps,'options'|'value'|'onChange' > {
+  value: number | string | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
 }
+
+export const IdSelect = (props: IdSelectProps) => {
+  const {
+    value,
+    onChange,
+    defaultOptionName = "",
+    options = [],
+    ...restProps
+  } = props;
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+      {...restProps}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options.map((option) => (
+        <Select.Option value={option.id} key={option.id}>
+          {option.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
